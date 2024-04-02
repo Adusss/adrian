6b6645599f3193ae65b9d5022b259d36bf2638a8
Adusss <79541872+Adusss@users.noreply.github.com>
2024-04-02 22:27:00 +0200

    Add files via upload

---
+++ icon.js
@@ -0,0 +1,456 @@
+const testDiv = document.getElementById("icon-img");
+
+const iconimg = [
+  {
+    icon_img: "img/1.jpg",
+    icon_num: "1",
+  },
+  {
+    icon_img: "img/2.png",
+    icon_num: "2",
+
+  },
+  {
+    icon_img: "img/3.png",
+    icon_num: "3",
+
+  },
+  {
+    icon_img: "img/4.png",
+    icon_num: "4",
+
+  },
+  {
+    icon_img: "img/5.png",
+    icon_num: "5",
+
+  },
+
+
+
+  {
+    icon_img: "img/6.png",
+    icon_num: "6",
+
+  },
+  {
+    icon_img: "img/7.png",
+    icon_num: "7",
+
+  },
+  {
+    icon_img: "img/8.png",
+    icon_num: "8",
+    
+  },
+  {
+    icon_img: "img/9.png",
+    icon_num: "9",
+
+  },
+  {
+    icon_img: "img/10.png",
+    icon_num: "10",
+
+  },
+
+
+
+  
+
+  {
+    icon_img: "img/11.png",
+    icon_num: "11",
+
+  },
+  {
+    icon_img: "img/12.png",
+    icon_num: "12",
+
+  },
+  {
+    icon_img: "img/13.png",
+    icon_num: "13",
+
+  },
+  {
+    icon_img: "img/14.png",
+    icon_num: "14",
+
+  },
+  {
+    icon_img: "img/15.png",
+    icon_num: "15",
+
+  },
+
+
+
+
+  {
+    icon_img: "img/16.png",
+    icon_num: "16",
+
+  },
+  {
+    icon_img: "img/17.png",
+    icon_num: "17",
+
+  },
+  {
+    icon_img: "img/18.png",
+    icon_num: "18",
+
+  },
+  {
+    icon_img: "img/19.png",
+    icon_num: "19",
+
+  },
+  {
+    icon_img: "img/20.png",
+    icon_num: "20",
+
+  },
+
+
+
+  
+  {
+    icon_img: "img/21.png",
+    icon_num: "21",
+
+  },
+  {
+    icon_img: "img/22.png",
+    icon_num: "22",
+
+  },
+  {
+    icon_img: "img/23.png",
+    icon_num: "23",
+
+  },
+  {
+    icon_img: "img/24.png",
+    icon_num: "24",
+
+  },
+  {
+    icon_img: "img/25.png",
+    icon_num: "25",
+
+  },
+
+
+
+  
+  {
+    icon_img: "img/26.png",
+    icon_num: "26",
+
+  },
+  {
+    icon_img: "img/27.png",
+    icon_num: "27",
+
+  },
+  {
+    icon_img: "img/28.png",
+    icon_num: "28",
+
+  },
+  {
+    icon_img: "img/29.png",
+    icon_num: "29",
+
+  },
+  {
+    icon_img: "img/30.png",
+    icon_num: "30",
+
+  },
+
+
+
+  
+  {
+    icon_img: "img/31.png",
+    icon_num: "31",
+
+  },
+  {
+    icon_img: "img/32.png",
+    icon_num: "32",
+
+  },
+  {
+    icon_img: "img/33.png",
+    icon_num: "33",
+
+  },
+  {
+    icon_img: "img/34.png",
+    icon_num: "34",
+
+  },
+  {
+    icon_img: "img/35.png",
+    icon_num: "35",
+
+  },
+
+
+
+  
+  {
+    icon_img: "img/36.png",
+    icon_num: "36",
+
+  },
+  {
+    icon_img: "img/37.png",
+    icon_num: "37",
+
+  },
+  {
+    icon_img: "img/38.png",
+    icon_num: "38",
+
+  },
+  {
+    icon_img: "img/39.png",
+    icon_num: "39",
+
+  },
+  {
+    icon_img: "img/40.png",
+    icon_num: "40",
+
+  },
+
+
+
+  
+  {
+    icon_img: "img/41.png",
+    icon_num: "41",
+
+  },
+  {
+    icon_img: "img/42.png",
+    icon_num: "42",
+
+  },
+  {
+    icon_img: "img/43.png",
+    icon_num: "43",
+
+  },
+  {
+    icon_img: "img/44.png",
+    icon_num: "44",
+
+  },
+  {
+    icon_img: "img/45.png",
+    icon_num: "45",
+
+  },
+
+
+
+  
+  {
+    icon_img: "img/46.png",
+    icon_num: "46",
+
+  },
+  {
+    icon_img: "img/47.png",
+    icon_num: "47",
+
+  },
+  {
+    icon_img: "img/48.png",
+    icon_num: "48",
+
+  },
+  {
+    icon_img: "img/49.png",
+    icon_num: "49",
+
+  },
+  {
+    icon_img: "img/50.png",
+    icon_num: "50",
+
+  },
+
+
+
+  
+  {
+    icon_img: "img/51.png",
+    icon_num: "51",
+
+  },
+  {
+    icon_img: "img/52.png",
+    icon_num: "52",
+
+  },
+  {
+    icon_img: "img/53.png",
+    icon_num: "53",
+
+  },
+  {
+    icon_img: "img/54.png",
+    icon_num: "54",
+
+  },
+  {
+    icon_img: "img/55.png",
+    icon_num: "55",
+
+  },
+
+
+
+
+  {
+    icon_img: "img/56.png",
+    icon_num: "56",
+
+  },
+  {
+    icon_img: "img/57.png",
+    icon_num: "57",
+
+  },
+  {
+    icon_img: "img/58.png",
+    icon_num: "58",
+
+  },
+  {
+    icon_img: "img/59.png",
+    icon_num: "59",
+
+  },
+  {
+    icon_img: "img/60.png",
+    icon_num: "60",
+
+  },
+
+
+
+
+  {
+    icon_img: "img/61.png",
+    icon_num: "61",
+
+  },
+  {
+    icon_img: "img/62.png",
+    icon_num: "62",
+
+  },
+  {
+    icon_img: "img/63.png",
+    icon_num: "63",
+
+  },
+  {
+    icon_img: "img/64.png",
+    icon_num: "64",
+
+  },
+  {
+    icon_img: "img/65.png",
+    icon_num: "65",
+
+  },
+
+
+
+
+  {
+    icon_img: "img/66.png",
+    icon_num: "66",
+
+  },
+  {
+    icon_img: "img/67.png",
+    icon_num: "67",
+
+  },
+  {
+    icon_img: "img/68.png",
+    icon_num: "68",
+
+  },
+  {
+    icon_img: "img/69.png",
+    icon_num: "69",
+
+  },
+  {
+    icon_img: "img/70.png",
+    icon_num: "70",
+
+  },
+
+
+
+
+  {
+    icon_img: "img/71.png",
+    icon_num: "71",
+
+  },
+  {
+    icon_img: "img/72.png",
+    icon_num: "72",
+
+  },
+  {
+    icon_img: "img/73.png",
+    icon_num: "73",
+
+  },
+  {
+    icon_img: "img/74.png",
+    icon_num: "74",
+
+  },
+  {
+    icon_img: "img/75.png",
+    icon_num: "75",
+
+  },
+
+
+
+  
+
+
+  
+
+  
+  
+
+
+];
+
+for (icon of iconimg) {
+  testDiv.innerHTML += `
+    <div class="icon-box"><img class="icon-img" src="${icon.icon_img}" alt="">
+    <div class="number-icon-box">
+        <div class="number-icon">${icon.icon_num}</div>
+    </div>
+</div>
+  `;
+}
