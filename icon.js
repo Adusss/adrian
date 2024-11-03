e40ef67058eaeeafffd727002affcc914cb156c5
Adusss <79541872+Adusss@users.noreply.github.com>
2024-11-03 22:18:23 +0000

    update

--- icon.js
+++ icon.js
@@ -1,24 +1,26 @@
-const testDiv = document.getElementById("icon-img");
+window.addEventListener("DOMContentLoaded", function () {
+  const testDiv = document.getElementById("icon-img");
 
-const iconimg = [];
-for (let i = 1; i <= 151; i++) {
-  iconimg.push({
-    icon_img: `img/${i}.png`,
-    icon_num: `${i}`,
-  });
-}
+  const iconimg = [];
+  for (let i = 1; i <= 151; i++) {
+    iconimg.push({
+      icon_img: `img/${i}.png`,
+      icon_num: `${i}`,
+    });
+  }
 
-let htmlContent = "";
+  let htmlContent = "";
 
-for (let icon of iconimg) {
-  htmlContent += `
-    <div class="icon-box">
-      <img class="icon-img" src="${icon.icon_img}" alt="Ikona ${icon.icon_num}">
-      <div class="number-icon-box">
-        <div class="number-icon">${icon.icon_num}</div>
+  for (let icon of iconimg) {
+    htmlContent += `
+      <div class="icon-box">
+        <img class="icon-img" src="${icon.icon_img}" alt="Ikona ${icon.icon_num}">
+        <div class="number-icon-box">
+          <div class="number-icon">${icon.icon_num}</div>
+        </div>
       </div>
-    </div>
-  `;
-}
+    `;
+  }
 
-testDiv.innerHTML = htmlContent;
+  testDiv.innerHTML = htmlContent;
+});
